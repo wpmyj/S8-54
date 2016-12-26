52df2cde2e275f0fb1fe347c92076c1a79142c63
Sasha7b9 <Sasha7b9@tut.by>
2016-12-26 08:46:42 +0300

    Удаление мусора из скрипта

--- delAndZIP_OSCI.py
+++ delAndZIP_OSCI.py
@@ -20,12 +20,3 @@ os.system('del sources\\VS-OSCI\\ControllerETH\obj\\*.* /s /q')
 os.system('del sources\\VS-OSCI\\ControllerUSB\bin\\*.* /s /q')
 os.system('del sources\\VS-OSCI\\ControllerUSB\obj\\*.* /s /q')
 
-
-date = datetime.datetime.now()
-
-name = 'Archiv\\S8-53-54' + date.strftime("_%Y_%m_%d_%H_%M_%S") + '.zip'
-os.system('7z a ' + name + ' sources')
-os.system('copy ' + name + ' h:work\\')
-os.system('copy ' + name + ' g:\\')
-
-os.system('pause')
