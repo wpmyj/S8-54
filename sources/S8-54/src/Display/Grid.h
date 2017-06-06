4b120247f7fca4eabd10d7ada3f8f7e2508b3293
Sasha7b9 <Sasha7b9@tut.by>
2017-06-06 09:31:41 +0300

    Косметика

--- sources/S8-54/src/Display/Grid.h
+++ sources/S8-54/src/Display/Grid.h
@@ -10,37 +10,37 @@
  *  @{
  */
 
-int     GridLeft(void);
+int GridLeft(void);
 
-int     GridRight(void);
+int GridRight(void);
 
-int     GridWidth(void);
+int GridWidth(void);
 
-float   GridDeltaY(void);
+float GridDeltaY(void);
 
-float   GridDeltaX(void);
+float GridDeltaX(void);
 
-int     GridWidthInCells(void);
+int GridWidthInCells(void);
 
-int     GridFullBottom(void);
+int GridFullBottom(void);
 
-int     GridFullHeight(void);
+int GridFullHeight(void);
 
-int     GridFullCenterHeight(void);
+int GridFullCenterHeight(void);
 
-int     GridChannelBottom(void);
+int GridChannelBottom(void);
 
-int     GridChannelHeight(void);
+int GridChannelHeight(void);
 
-int     GridChannelCenterHeight(void);
+int GridChannelCenterHeight(void);
 
-int     GridMathHeight(void);
+int GridMathHeight(void);
 
-int     GridMathBottom(void);
+int GridMathBottom(void);
 
-int     GridMathTop(void);
+int GridMathTop(void);
 
-int     BottomMessages(void);
+int BottomMessages(void);
 
 void CoordTrigLevel(int *left, int *top, int width);
 
