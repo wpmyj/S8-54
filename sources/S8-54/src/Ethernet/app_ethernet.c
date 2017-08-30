e530f5ea660338e7c597da966a90a39008471df7
Sasha <Sasha7b9@tut.by>
2017-08-30 23:32:50 +0300

    Рефакторинг

--- sources/S8-54/src/Ethernet/app_ethernet.c
+++ sources/S8-54/src/Ethernet/app_ethernet.c
@@ -1,33 +1,3 @@
-// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-/**
-  ******************************************************************************
-  * @file    LwIP/LwIP_HTTP_Server_Raw/Src/app_ethernet.c 
-  * @author  MCD Application Team
-  * @version V1.4.1
-  * @date    09-October-2015
-  * @brief   Ethernet specefic module
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2015 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "lwip/opt.h"
 #include "main.h"
 #include "lwip/dhcp.h"
@@ -35,192 +5,41 @@
 #include "ethernetif.h"
 #include "globals.h"
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-#ifdef USE_DHCP
-#define MAX_DHCP_TRIES  4
-uint32_t DHCPfineTimer = 0;
-__IO uint8_t DHCP_state;
-#endif
 
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
-/**
-  * @brief  Notify the User about the nework interface config status 
-  * @param  netif: the network interface
-  * @retval None
-  */
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void User_notification(struct netif *netif) 
 {
-  if (netif_is_up(netif))
- {
-#ifdef USE_DHCP
-    /* Update DHCP state machine */
-    DHCP_state = DHCP_START;
-#endif /* USE_DHCP */
- }
- else
-  {  
-#ifdef USE_DHCP
-    /* Update DHCP state machine */
-    DHCP_state = DHCP_LINK_DOWN;
-#endif  /* USE_DHCP */
-  } 
+    if (netif_is_up(netif))
+    {
+    }
+    else
+    {
+    }
 }
 
-/**
-  * @brief  This function notify user about link status changement.
-  * @param  netif: the network interface
-  * @retval None
-  */
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 void ethernetif_notify_conn_changed(struct netif *netif)
 {
-  ip_addr_t ipaddr;
-  ip_addr_t netmask;
-  ip_addr_t gw;
+    ip_addr_t ipaddr;
+    ip_addr_t netmask;
+    ip_addr_t gw;
 
-  if(netif_is_link_up(netif))
-  {
-#ifdef USE_DHCP
-    /* Update DHCP state machine */
-    DHCP_state = DHCP_START;
-#else
-    IP4_ADDR(&ipaddr, IP_ADDR0, IP_ADDR1, IP_ADDR2, IP_ADDR3);
-    IP4_ADDR(&netmask, NETMASK_ADDR0, NETMASK_ADDR1 , NETMASK_ADDR2, NETMASK_ADDR3);
-    IP4_ADDR(&gw, GW_ADDR0, GW_ADDR1, GW_ADDR2, GW_ADDR3);    
-#endif /* USE_DHCP */   
-    
-    netif_set_addr(netif, &ipaddr , &netmask, &gw);
-    
-    /* When the netif is fully configured this function must be called.*/
-    netif_set_up(netif);
-    CABLE_LAN_IS_CONNECTED = true;
-  }
-  else
-  {
-#ifdef USE_DHCP
-    /* Update DHCP state machine */
-    DHCP_state = DHCP_LINK_DOWN;
-#endif /* USE_DHCP */
-    
-    /*  When the netif link is down this function must be called.*/
-    netif_set_down(netif);
-    CABLE_LAN_IS_CONNECTED = false;
-  }
-}
-
-#ifdef USE_DHCP
-/**
-  * @brief  DHCP_Process_Handle
-  * @param  None
-  * @retval None
-  */
-void DHCP_Process(struct netif *netif)
-{
-  struct ip_addr ipaddr;
-  struct ip_addr netmask;
-  struct ip_addr gw;
-  uint32_t IPaddress = 0;
-  
-  switch (DHCP_state)
-  {
-  case DHCP_START:
+    if (netif_is_link_up(netif))
     {
-      netif->ip_addr.addr = 0;
-      netif->netmask.addr = 0;
-      netif->gw.addr = 0;
-      IPaddress = 0;
-      DHCP_state = DHCP_WAIT_ADDRESS;
-      dhcp_start(netif);
-#ifdef USE_LCD
-      LCD_UsrLog ("  State: Looking for DHCP sever ...\n");
-#endif
+        IP4_ADDR(&ipaddr, IP_ADDR0, IP_ADDR1, IP_ADDR2, IP_ADDR3);
+        IP4_ADDR(&netmask, NETMASK_ADDR0, NETMASK_ADDR1 , NETMASK_ADDR2, NETMASK_ADDR3);
+        IP4_ADDR(&gw, GW_ADDR0, GW_ADDR1, GW_ADDR2, GW_ADDR3);    
+
+        netif_set_addr(netif, &ipaddr, &netmask, &gw);
+    
+        /* When the netif is fully configured this function must be called.*/
+        netif_set_up(netif);
+        CABLE_LAN_IS_CONNECTED = true;
     }
-    break;
-
-  case DHCP_WAIT_ADDRESS:
-    {
-      /* Read the new IP address */
-      IPaddress = netif->ip_addr.addr;
-      
-      if (IPaddress !=0) 
-      {
-        DHCP_state = DHCP_ADDRESS_ASSIGNED;	
-        
-        /* Stop DHCP */
-        dhcp_stop(netif);
-
-#ifdef USE_LCD 
-        uint8_t iptab[4];
-        uint8_t iptxt[20];
-  
-        iptab[0] = (uint8_t)(IPaddress >> 24);
-        iptab[1] = (uint8_t)(IPaddress >> 16);
-        iptab[2] = (uint8_t)(IPaddress >> 8);
-        iptab[3] = (uint8_t)(IPaddress);
-
-        sprintf((char *)iptxt, "%d.%d.%d.%d", iptab[3], iptab[2], iptab[1], iptab[0]);       
-
-        LCD_UsrLog ("IP address assigned by a DHCP server: %s\n", iptxt);
-#else
-     BSP_LED_On(LED1);   
-#endif
-      }
-      else
-      {
-        /* DHCP timeout */
-        if (netif->dhcp->tries > MAX_DHCP_TRIES)
-        {
-          DHCP_state = DHCP_TIMEOUT;
-
-          /* Stop DHCP */
-          dhcp_stop(netif);
-
-          /* Static address used */
-          IP4_ADDR(&ipaddr, IP_ADDR0 ,IP_ADDR1 , IP_ADDR2 , IP_ADDR3 );
-          IP4_ADDR(&netmask, NETMASK_ADDR0, NETMASK_ADDR1, NETMASK_ADDR2, NETMASK_ADDR3);
-          IP4_ADDR(&gw, GW_ADDR0, GW_ADDR1, GW_ADDR2, GW_ADDR3);
-          netif_set_addr(netif, &ipaddr , &netmask, &gw);
-
-#ifdef USE_LCD  
-          uint8_t iptxt[20];
-          
-          sprintf((char *)iptxt, "%d.%d.%d.%d", IP_ADDR0, IP_ADDR1, IP_ADDR2, IP_ADDR3);
-          LCD_UsrLog ("DHCP timeout !!\n");
-          LCD_UsrLog ("Static IP address  : %s\n", iptxt);      
-#else
-     BSP_LED_On(LED1);  
-#endif
-        }
-      }
+    else
+    {   
+        /*  When the netif link is down this function must be called.*/
+        netif_set_down(netif);
+        CABLE_LAN_IS_CONNECTED = false;
     }
-    break;
-  default: break;
-  }
 }
-
-/**
-  * @brief  DHCP periodic check
-  * @param  localtime the current LocalTime value
-  * @retval None
-  */
-void DHCP_Periodic_Handle(struct netif *netif)
-{  
-  /* Fine DHCP periodic process every 500ms */
-  if (HAL_GetTick() - DHCPfineTimer >= DHCP_FINE_TIMER_MSECS)
-  {
-    DHCPfineTimer =  HAL_GetTick();
-    if ((DHCP_state != DHCP_ADDRESS_ASSIGNED) && 
-        (DHCP_state != DHCP_TIMEOUT) &&
-          (DHCP_state != DHCP_LINK_DOWN))
-    {
-      /* process DHCP state machine */
-      DHCP_Process(netif);    
-    }
-  }
-}
-#endif
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
