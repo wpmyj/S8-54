1a6f2ec7a0dbe7145028b2f1c5da0e111b1464b0
Sasha7b9 <Sasha7b9@tut.by>
2017-11-01 13:52:10 +0300

    Начало перехода на С++ usbh_conf.c

--- sources/S8-54/src/libs/usbh_conf.c
+++ sources/S8-54/src/libs/usbh_conf.c
@@ -1,5 +1,3 @@
-
-
 #include "defines.h"
 #include "Globals.h"
 #include <stm32f4xx_hal.h>
@@ -19,7 +17,7 @@
   */
 void HAL_HCD_SOF_Callback(HCD_HandleTypeDef *hhcd)
 {
-  USBH_LL_IncTimer (hhcd->pData);
+  USBH_LL_IncTimer ((USBH_HandleTypeDef *)hhcd->pData);
 }
 
 /**
@@ -29,7 +27,7 @@ void HAL_HCD_SOF_Callback(HCD_HandleTypeDef *hhcd)
   */
 void HAL_HCD_Connect_Callback(HCD_HandleTypeDef *hhcd)
 {
-  USBH_LL_Connect(hhcd->pData);
+  USBH_LL_Connect((USBH_HandleTypeDef *)hhcd->pData);
 }
 
 /**
@@ -39,7 +37,7 @@ void HAL_HCD_Connect_Callback(HCD_HandleTypeDef *hhcd)
   */
 void HAL_HCD_Disconnect_Callback(HCD_HandleTypeDef *hhcd)
 {
-  USBH_LL_Disconnect(hhcd->pData);
+  USBH_LL_Disconnect((USBH_HandleTypeDef *)hhcd->pData);
 } 
 
 /**
@@ -94,7 +92,7 @@ USBH_StatusTypeDef USBH_LL_Init(USBH_HandleTypeDef *phost)
   */
 USBH_StatusTypeDef USBH_LL_DeInit(USBH_HandleTypeDef *phost)
 {
-  HAL_HCD_DeInit(phost->pData);
+  HAL_HCD_DeInit((HCD_HandleTypeDef *)phost->pData);
   return USBH_OK; 
 }
 
@@ -105,7 +103,7 @@ USBH_StatusTypeDef USBH_LL_DeInit(USBH_HandleTypeDef *phost)
   */
 USBH_StatusTypeDef USBH_LL_Start(USBH_HandleTypeDef *phost)
 {
-  HAL_HCD_Start(phost->pData);
+  HAL_HCD_Start((HCD_HandleTypeDef *)phost->pData);
   return USBH_OK; 
 }
 
@@ -116,7 +114,7 @@ USBH_StatusTypeDef USBH_LL_Start(USBH_HandleTypeDef *phost)
   */
 USBH_StatusTypeDef USBH_LL_Stop(USBH_HandleTypeDef *phost)
 {
-  HAL_HCD_Stop(phost->pData);
+  HAL_HCD_Stop((HCD_HandleTypeDef *)phost->pData);
   return USBH_OK; 
 }
 
@@ -129,7 +127,7 @@ USBH_SpeedTypeDef USBH_LL_GetSpeed(USBH_HandleTypeDef *phost)
 {
   USBH_SpeedTypeDef speed = USBH_SPEED_FULL;
     
-  switch (HAL_HCD_GetCurrentSpeed(phost->pData))
+  switch (HAL_HCD_GetCurrentSpeed((HCD_HandleTypeDef *)phost->pData))
   {
   case 0: 
     speed = USBH_SPEED_HIGH;
@@ -157,7 +155,7 @@ USBH_SpeedTypeDef USBH_LL_GetSpeed(USBH_HandleTypeDef *phost)
   */
 USBH_StatusTypeDef USBH_LL_ResetPort (USBH_HandleTypeDef *phost) 
 {
-  HAL_HCD_ResetPort(phost->pData);
+  HAL_HCD_ResetPort((HCD_HandleTypeDef *)phost->pData);
   return USBH_OK; 
 }
 
@@ -169,7 +167,7 @@ USBH_StatusTypeDef USBH_LL_ResetPort (USBH_HandleTypeDef *phost)
   */
 uint32_t USBH_LL_GetLastXferSize(USBH_HandleTypeDef *phost, uint8_t pipe)  
 {
-  return HAL_HCD_HC_GetXferCount(phost->pData, pipe);
+  return HAL_HCD_HC_GetXferCount((HCD_HandleTypeDef *)phost->pData, pipe);
 }
 
 /**
@@ -191,7 +189,7 @@ USBH_StatusTypeDef USBH_LL_OpenPipe(USBH_HandleTypeDef *phost,
                                     uint8_t ep_type,
                                     uint16_t mps)
 {
-  HAL_HCD_HC_Init(phost->pData,
+  HAL_HCD_HC_Init((HCD_HandleTypeDef *)phost->pData,
                   pipe,
                   epnum,
                   dev_address,
@@ -209,7 +207,7 @@ USBH_StatusTypeDef USBH_LL_OpenPipe(USBH_HandleTypeDef *phost,
   */
 USBH_StatusTypeDef USBH_LL_ClosePipe(USBH_HandleTypeDef *phost, uint8_t pipe)   
 {
-  HAL_HCD_HC_Halt(phost->pData, pipe);
+  HAL_HCD_HC_Halt((HCD_HandleTypeDef *)phost->pData, pipe);
   return USBH_OK;
 }
 
@@ -249,7 +247,7 @@ USBH_StatusTypeDef USBH_LL_SubmitURB(USBH_HandleTypeDef *phost,
                                      uint16_t length,
                                      uint8_t do_ping) 
 {
-  HAL_HCD_HC_SubmitRequest(phost->pData,
+  HAL_HCD_HC_SubmitRequest((HCD_HandleTypeDef *)phost->pData,
                            pipe, 
                            direction,
                            ep_type,  
@@ -276,7 +274,7 @@ USBH_StatusTypeDef USBH_LL_SubmitURB(USBH_HandleTypeDef *phost,
   */
 USBH_URBStateTypeDef USBH_LL_GetURBState(USBH_HandleTypeDef *phost, uint8_t pipe) 
 {
-  return (USBH_URBStateTypeDef)HAL_HCD_HC_GetURBState (phost->pData, pipe);
+  return (USBH_URBStateTypeDef)HAL_HCD_HC_GetURBState ((HCD_HandleTypeDef *)phost->pData, pipe);
 }
 
 /**
